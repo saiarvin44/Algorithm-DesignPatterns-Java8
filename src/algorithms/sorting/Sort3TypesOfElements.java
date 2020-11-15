f89d237848a3c409bd32c706d895b756b2b1e4e6
saiarvin44 <saiarvind44@gmail.com>
2020-11-15 20:16:30 +0530

    chore: sort algos implemented in diff varieties

---
+++ src/algorithms/sorting/Sort3TypesOfElements.java
@@ -0,0 +1,36 @@
+package algorithms.sorting;
+
+public class Sort3TypesOfElements {
+  public static void main(String[] args) {
+    // Dusnational algorithm
+    int[] arr = new int[] {0, 1, 2, 1, 1, 2};
+    sort3Types(arr, arr.length);
+    for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+  }
+
+  private static void sort3Types(int[] arr, int n) {
+    int lo = 0, hi = n - 1, mid = 0;
+    while (mid <= hi) {
+      switch (arr[mid]) {
+        case 0:
+          swapElts(arr, lo, mid);
+          lo++;
+          mid++;
+          break;
+        case 1:
+          mid++;
+          break;
+        case 2:
+          swapElts(arr, mid, hi);
+          hi--;
+          break;
+      }
+    }
+  }
+
+  private static void swapElts(int[] arr, int lo, int mid) {
+    int temp = arr[lo];
+    arr[lo] = arr[mid];
+    arr[mid] = temp;
+  }
+}
