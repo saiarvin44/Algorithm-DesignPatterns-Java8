c539c7959f5a0b48e26b65db3b5d2fe9965d177d
saiarvin44 <saiarvind44@gmail.com>
2020-12-07 22:49:22 +0530

    Chore: hashing

---
+++ src/algorithms/hashing/Union2ArraysCount.java
@@ -0,0 +1,7 @@
+package algorithms.hashing;
+
+public class Union2ArraysCount {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/algorithms/hashing/chaining.java
@@ -0,0 +1,32 @@
+package algorithms.hashing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class chaining {
+  int bucket;
+  ArrayList<LinkedList<Integer>> table;
+
+  chaining(int b) {
+    bucket = b;
+    table = new ArrayList<LinkedList<Integer>>();
+    for (int i = 0; i < bucket; i++) table.add(new LinkedList<Integer>());
+  }
+
+  public static void main(String[] args) {}
+
+  void insert(int key) {
+    int i = key % bucket;
+    this.table.get(i).add(key);
+  }
+
+  void remove(int key) {
+    int i = key % bucket;
+    this.table.get(i).remove((Integer) i);
+  }
+
+  boolean search(int key) {
+    int i = key % bucket;
+    return this.table.get(i).contains(key);
+  }
+}
