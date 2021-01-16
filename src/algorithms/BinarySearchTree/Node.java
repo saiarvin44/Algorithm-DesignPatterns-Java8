859a7458da58a380fcf1ea9329507858c7126f2b
saiarvin44 <saiarvind44@gmail.com>
2021-01-16 22:46:34 +0530

    chore: binary search tree

---
+++ src/algorithms/BinarySearchTree/Node.java
@@ -0,0 +1,12 @@
+package algorithms.BinarySearchTree;
+
+import algorithms.tree.Representation;
+
+public class Node {
+    int key;
+    Node left, right;
+
+    Node(int key) {
+        this.key = key;
+    }
+}
