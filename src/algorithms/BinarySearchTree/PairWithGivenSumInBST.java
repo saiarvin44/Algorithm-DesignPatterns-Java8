a75079c2764a94edf51458d639e104a60134aefe
saiarvin44 <saiarvind44@gmail.com>
2021-01-18 08:32:17 +0530

    chore: binary search tree

---
+++ src/algorithms/BinarySearchTree/PairWithGivenSumInBST.java
@@ -0,0 +1,13 @@
+package algorithms.BinarySearchTree;
+
+import java.util.HashSet;
+
+public class PairWithGivenSumInBST {
+  boolean isPairSum(Node root, int sum, HashSet<Integer> s) {
+    if (root == null) return false;
+    if (isPairSum(root.left, sum, s) == true) return true;
+    if (s.contains(sum - root.key)) return true;
+    else s.add(root.key);
+    return isPairSum(root.right, sum, s);
+  }
+}
---
+++ src/algorithms/BinarySearchTree/PrintKthSmallest.java
@@ -0,0 +1,13 @@
+package algorithms.BinarySearchTree;
+
+public class PrintKthSmallest {
+  public void printKth(Node root, int k) {
+    int count = 0;
+    if (root != null) {
+      printKth(root.left, k);
+      count++;
+      if (count == k) System.out.print(root.key);
+      printKth(root.right, k);
+    }
+  }
+}
